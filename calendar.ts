8051a784aa3cc9b0855158dbddb8511c28ad32d6
Suzu Tomo <47011206+kamekyame@users.noreply.github.com>
2021-04-01 02:57:44 +0900

    文字列からVCalendarオブジェクトを生成できるように変更

--- calendar.ts
+++ calendar.ts
@@ -10,13 +10,13 @@ function toISOString(date: Date, allDay = false) {
 
   //console.log(date);
 
-  let str = date.getFullYear().toString() +
-    pad(date.getMonth() + 1) +
-    pad(date.getDate());
+  let str = date.getUTCFullYear().toString() +
+    pad(date.getUTCMonth() + 1) +
+    pad(date.getUTCDate());
   if (!allDay) {
-    str += "T" + pad(date.getHours()) +
-      pad(date.getMinutes()) +
-      pad(date.getSeconds());
+    str += "T" + pad(date.getUTCHours()) +
+      pad(date.getUTCMinutes()) +
+      pad(date.getUTCSeconds()) + "Z";
   }
 
   //console.log(str);
@@ -24,15 +24,49 @@ function toISOString(date: Date, allDay = false) {
   return str;
 }
 
+function BetweenBeginEnd(list: string[][], s: number, e: number) {
+  let value = "";
+  let begin = s;
+  for (; begin < e; begin++) {
+    if (list[begin][0] === "BEGIN") {
+      value = list[begin][1];
+      break;
+    }
+  }
+  let end = begin;
+  for (; end < e; end++) {
+    if (list[end][0] === "END" && list[end][1] === value) break;
+  }
+  return { begin, end, value };
+}
+
+function dateParse(text: string) {
+  const year = parseInt(text.slice(0, 4));
+  const month = parseInt(text.slice(4, 6)) - 1;
+  const day = parseInt(text.slice(6, 8));
+  const hour = parseInt(text.slice(9, 11) || "0");
+  const minite = parseInt(text.slice(11, 13) || "0");
+  const second = parseInt(text.slice(13, 15) || "0");
+  const date = new Date(0);
+  date.setUTCFullYear(year, month, day);
+  date.setUTCHours(hour, minite, second);
+  return date;
+}
+
 export class VCalendar {
   private version = "2.0";
   private prodId: string;
   private timezone?: VTimezone;
-  private events: VEvent[] = [];
+  private events: VEvent[];
 
-  constructor({ prodId, timezone }: { prodId: string; timezone?: VTimezone }) {
+  constructor({ prodId, timezone, events }: {
+    prodId: string;
+    timezone?: VTimezone;
+    events?: VEvent[];
+  }) {
     this.prodId = prodId;
     this.timezone = timezone;
+    this.events = events || [];
   }
 
   addEvent(event: VEvent) {
@@ -50,7 +84,51 @@ export class VCalendar {
     str += "END:VCALENDAR\r\n";
     return str;
   }
+
+  static convertICS(text: string) {
+    const lines = text.split("\r\n").map((e) => e.split(":"));
+    console.log(lines);
+
+    // VCALENCARのBEGINとENDを取る
+    let { begin: b, end: e, value: v } = BetweenBeginEnd(
+      lines,
+      0,
+      lines.length,
+    );
+    if (v !== "VCALENDAR") return;
+    console.log(b, e);
+
+    let prodId: string | undefined;
+    let timezone: VTimezone | undefined = undefined;
+    const events: VEvent[] = [];
+
+    while (true) {
+      const { begin: b1, end: e1, value: v1 } = BetweenBeginEnd(
+        lines,
+        b + 1,
+        e - 1,
+      );
+      console.log(b1, e1, v1);
+      if (b1 === e1) break;
+      for (let i = b + 1; i < b1; i++) {
+        if (lines[i][0] === "PRODID") prodId = lines[i][1];
+      }
+      if (v1 === "VTIMEZONE") {
+        timezone = VTimezone.convertICS(lines.slice(b1, e1 + 1));
+        //console.log("VTIMEZONE");
+      } else if (v1 === "VEVENT") {
+        const event = VEvent.convertICS(lines.slice(b1, e1 + 1));
+        //console.log(event);
+        if (event) events.push(event);
+      }
+      b = e1;
+    }
+    if (!prodId) return;
+    const calendar = new VCalendar({ prodId, timezone, events });
+    return calendar;
+  }
 }
+
 export class VTimezone {
   private tzId: string;
   private standard: Standard;
@@ -67,19 +145,48 @@ export class VTimezone {
     str += "END:VTIMEZONE\r\n";
     return str;
   }
+
+  static convertICS(list: string[][]) {
+    let tzId: string | undefined;
+    let standard: Standard | undefined = undefined;
+
+    let b = 0;
+    const e = list.length;
+    while (true) {
+      const { begin: b1, end: e1, value: v1 } = BetweenBeginEnd(
+        list,
+        b + 1,
+        e - 1,
+      );
+      console.log(b1, e1, v1);
+      if (b1 === e1) break;
+      for (let i = b + 1; i < b1; i++) {
+        if (list[i][0] === "TZID") tzId = list[i][1];
+      }
+      if (v1 === "STANDARD") {
+        standard = Standard.convertICS(list.slice(b1, e1 + 1)); // console.log("STANDARD");
+      }
+      b = e1;
+    }
+    if (!tzId || !standard) return;
+    const timezone = new VTimezone({ tzId, standard });
+    return timezone;
+  }
 }
 
 export class Standard {
-  private dtStart: Date = new Date(0);
+  private dtStart: Date;
   private tzOffsetFrom: string;
   private tzOffsetTo: string;
 
   constructor(
-    { tzOffsetFrom, tzOffsetTo }: {
+    { dtStart, tzOffsetFrom, tzOffsetTo }: {
+      dtStart?: Date;
       tzOffsetFrom?: string;
       tzOffsetTo?: string;
     },
   ) {
+    this.dtStart = dtStart || new Date(0);
     this.tzOffsetFrom = tzOffsetFrom || "+0000";
     this.tzOffsetTo = tzOffsetTo || "+0000";
   }
@@ -92,6 +199,29 @@ export class Standard {
     str += "END:STANDARD\r\n";
     return str;
   }
+
+  static convertICS(list: string[][]) {
+    let dtStart = new Date(0);
+    let tzOffsetFrom = "+0000";
+    let tzOffsetTo = "+0000";
+
+    for (let i = 0; i < list.length; i++) {
+      switch (list[i][0]) {
+        case "DTSTART":
+          console.log(dateParse(list[i][1]));
+          dtStart = dateParse(list[i][1]);
+          break;
+        case "TZOFFSETFROM":
+          tzOffsetFrom = list[i][1];
+          break;
+        case "TZOFFSETTO":
+          tzOffsetTo = list[i][1];
+          break;
+      }
+    }
+    const standard = new Standard({ dtStart, tzOffsetFrom, tzOffsetTo });
+    return standard;
+  }
 }
 
 export class VEvent {
@@ -103,19 +233,21 @@ export class VEvent {
   private readonly uId: string;
 
   constructor(
-    { dtStart, dtEnd, summary, allDay }: {
+    { dtStart, dtEnd, summary, allDay, dtStamp, uId }: {
       dtStart: Date;
       dtEnd: Date;
       summary: string;
       allDay?: boolean;
+      dtStamp?: Date;
+      uId?: string;
     },
   ) {
     this.dtStart = dtStart;
     this.dtEnd = dtEnd;
     this.summary = summary;
-    this.dtStamp = new Date();
+    this.dtStamp = dtStamp || new Date();
     this.allDay = allDay || false;
-    this.uId = v4.generate();
+    this.uId = uId || v4.generate();
   }
 
   toICSString() {
@@ -129,5 +261,30 @@ export class VEvent {
     str += "UID:" + this.uId + "\r\n";
     str += "END:VEVENT" + "\r\n";
     return str;
+  }
+
+  static convertICS(list: string[][]) {
+    let dtStart: Date | undefined = undefined;
+    let dtEnd: Date | undefined = undefined;
+    let summary: string | undefined = undefined;
+    let dtStamp: Date | undefined = undefined;
+    let allDay = false;
+    let uId: string | undefined = undefined;
+
+    for (let i = 0; i < list.length; i++) {
+      const [k, v] = list[i];
+      if (k.startsWith("DTSTART")) {
+        dtStart = dateParse(v);
+        if (k.includes("VALUE=DATE")) allDay = true;
+      } else if (k.startsWith("DTEND")) {
+        dtEnd = dateParse(v);
+        if (k.includes("VALUE=DATE")) allDay = true;
+      } else if (k === "SUMMARY") summary = v;
+      else if (k === "DTSTAMP") dtStamp = dateParse(v);
+      else if (k === "UID") uId = v;
+    }
+    if (!dtStart || !dtEnd || !summary || !dtStamp || !uId) return;
+    const event = new VEvent({ dtStart, dtEnd, summary, dtStamp, uId, allDay });
+    return event;
   }
 }
