8e3f8a85bb8dffc957d40bb0df4f7b1cbd7e976c
Suzu Tomo <47011206+kamekyame@users.noreply.github.com>
2021-04-01 15:48:39 +0900

    カレンダー名、説明をicsに含めるように改良

--- util.ts
+++ util.ts
@@ -1,11 +1,15 @@
 import { Standard, VCalendar, VTimezone } from "./calendar.ts";
 
-export function createKosenCalendar() {
+export function createKosenCalendar(shortKosenName: string, kosenName: string) {
   const standard = new Standard({ tzOffsetFrom: "+0900", tzOffsetTo: "+0900" });
   const timezone = new VTimezone({ tzId: "Japan", standard });
   const calendar = new VCalendar({
     prodId: "KOSEN calender scraping project",
     timezone,
+    xWrCalName: shortKosenName + " - KOSEN calendar",
+    xWrCalDesc: kosenName + "の行事予定です。",
+    xWrTimezone: "Asia/Tokyo",
   });
+
   return calendar;
 }
