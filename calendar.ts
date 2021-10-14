f87d1b298372f7793ab885fb8da6f75739068cbc
Suzu Tomo <kamekyame@outlook.com>
2021-10-14 10:56:56 +0900

    Change: eol from crlf to lf

--- calendar.ts
+++ calendar.ts
@@ -108,23 +108,23 @@ export class VCalendar {
 
   toICSString() {
     this.sortingEvents();
-    let str = "BEGIN:VCALENDAR\r\n";
-    str += "VERSION:" + this.version + "\r\n";
-    str += "PRODID:" + this.prodId + "\r\n";
-    if (this.xWrCalName) str += "X-WR-CALNAME:" + this.xWrCalName + "\r\n";
-    if (this.xWrCalDesc) str += "X-WR-CALDESC:" + this.xWrCalDesc + "\r\n";
-    if (this.xWrTimezone) str += "X-WR-TIMEZONE:" + this.xWrTimezone + "\r\n";
-    str += "X-WR-RELCALID:" + this.xWrRelcalId + "\r\n";
+    let str = "BEGIN:VCALENDAR\n";
+    str += "VERSION:" + this.version + "\n";
+    str += "PRODID:" + this.prodId + "\n";
+    if (this.xWrCalName) str += "X-WR-CALNAME:" + this.xWrCalName + "\n";
+    if (this.xWrCalDesc) str += "X-WR-CALDESC:" + this.xWrCalDesc + "\n";
+    if (this.xWrTimezone) str += "X-WR-TIMEZONE:" + this.xWrTimezone + "\n";
+    str += "X-WR-RELCALID:" + this.xWrRelcalId + "\n";
     if (this.timezone) str += this.timezone.toICSString();
     this.events.forEach((e) => {
       str += e.toICSString();
     });
-    str += "END:VCALENDAR\r\n";
+    str += "END:VCALENDAR\n";
     return str;
   }
 
   static convertICS(text: string) {
-    const lines = text.split(/\r\n|\n|\r/).map((e) => e.split(":"));
+    const lines = text.split(/\n|\n|\r/).map((e) => e.split(":"));
     //console.log(lines);
 
     // VCALENCARのBEGINとENDを取る
@@ -192,10 +192,10 @@ export class VTimezone {
   }
 
   toICSString() {
-    let str = "BEGIN:VTIMEZONE\r\n";
-    str += "TZID:" + this.tzId + "\r\n";
+    let str = "BEGIN:VTIMEZONE\n";
+    str += "TZID:" + this.tzId + "\n";
     str += this.standard.toICSString();
-    str += "END:VTIMEZONE\r\n";
+    str += "END:VTIMEZONE\n";
     return str;
   }
 
@@ -245,11 +245,11 @@ export class Standard {
   }
 
   toICSString() {
-    let str = "BEGIN:STANDARD\r\n";
-    str += "DTSTART:" + toISOString(this.dtStart) + "\r\n";
-    str += "TZOFFSETFROM:" + this.tzOffsetFrom + "\r\n";
-    str += "TZOFFSETTO:" + this.tzOffsetTo + "\r\n";
-    str += "END:STANDARD\r\n";
+    let str = "BEGIN:STANDARD\n";
+    str += "DTSTART:" + toISOString(this.dtStart) + "\n";
+    str += "TZOFFSETFROM:" + this.tzOffsetFrom + "\n";
+    str += "TZOFFSETTO:" + this.tzOffsetTo + "\n";
+    str += "END:STANDARD\n";
     return str;
   }
 
@@ -304,15 +304,15 @@ export class VEvent {
   }
 
   toICSString() {
-    let str = "BEGIN:VEVENT\r\n";
+    let str = "BEGIN:VEVENT\n";
     str += "DTSTART" + (this.allDay ? ";VALUE=DATE:" : ":") +
-      toISOString(this.dtStart, this.allDay) + "\r\n";
+      toISOString(this.dtStart, this.allDay) + "\n";
     str += "DTEND" + (this.allDay ? ";VALUE=DATE:" : ":") +
-      toISOString(this.dtEnd, this.allDay) + "\r\n";
-    str += "SUMMARY:" + this.summary + "\r\n";
-    str += "DTSTAMP:" + toISOString(this.dtStamp) + "\r\n";
-    str += "UID:" + this.uId + "\r\n";
-    str += "END:VEVENT" + "\r\n";
+      toISOString(this.dtEnd, this.allDay) + "\n";
+    str += "SUMMARY:" + this.summary + "\n";
+    str += "DTSTAMP:" + toISOString(this.dtStamp) + "\n";
+    str += "UID:" + this.uId + "\n";
+    str += "END:VEVENT" + "\n";
     return str;
   }
 
