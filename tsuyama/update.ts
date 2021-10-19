56a62e410bc492b574a47390f741936d6de961f0
Suzu Tomo <kamekyame@outlook.com>
2021-10-19 09:35:22 +0900

    Add; 津山高専カレンダー

--- tsuyama/update.ts
+++ tsuyama/update.ts
@@ -1,25 +1,29 @@
 import { getDOM } from "../dom.ts";
 import { VCalendar, VEvent } from "../calendar.ts";
-import { createKosenCalendar } from "../util.ts";
+import {
+  compEvents,
+  createKosenCalendar,
+  fullToHalf,
+  resolver,
+} from "../util.ts";
 
-async function scraping(oldCalendar?: VCalendar) {
-  const calendar = oldCalendar || createKosenCalendar("津山高専", "津山工業高等専門学校");
+const resolve = resolver(import.meta);
+
+async function getScrapeEvents() {
+  const events: VEvent[] = [];
 
   const dom = await getDOM(
     "https://www.tsuyama-ct.ac.jp/gyoujiVer4/gyouji.html",
   );
-  if (!dom) return;
+  if (!dom) throw Error("Can not get dom");
   const body = dom.body;
 
   const contents = body.querySelector("#contents");
-  if (!contents) return;
+  if (!contents) throw Error("Can not get contents");
   // 令和○年度を西暦に変換
   const title = contents.querySelector("h3");
-  const yearText = title?.textContent.replace(
-    /[０-９]/g,
-    (s) => String.fromCharCode(s.charCodeAt(0) - 65248),
-  ).match(/\d+/);
-  if (!yearText) return;
+  const yearText = fullToHalf(title?.textContent).match(/\d+/);
+  if (!yearText) throw Error("Can not get year");
   const year = parseInt(yearText[0]) + 2019;
   console.log(year + "年度");
 
@@ -27,20 +31,63 @@ async function scraping(oldCalendar?: VCalendar) {
   monthUls.forEach((monthUl, i) => {
     const month = (i + 4) % 12;
     console.log(month + "月");
+
     const dayLi = monthUl.getElementsByTagName("li");
     dayLi.forEach((day) => {
-      const text = day.innerHTML;
-      text.match("");
-      console.log(day.innerHTML);
+      const rawText = day.textContent;
+      const [dayText, summariesText] = fullToHalf(rawText).trim().split(
+        " ",
+      );
 
-      // ここまで作ったけど2021年度のがないことに気づいたので後回しにします。
+      const daysText = dayText.split("・");
+      const summaries = summariesText.split("、");
+
+      console.log(rawText, daysText, summaries /*, match*/);
+
+      daysText.forEach((dayText) => {
+        const dayMatch = dayText.match(
+          /^(\d+)日（.+?）(?:～(?:(\d+?)月)?(\d+?)日（.+?）)?$/,
+        );
+        if (!dayMatch) return;
+        const [, fromDayText, toMonthText, toDayText] = dayMatch;
+        const fromDay = parseInt(fromDayText);
+        const toMonth = toMonthText ? parseInt(toMonthText) : month;
+        const toDay = toDayText ? parseInt(toDayText) : fromDay;
+        console.log(dayMatch);
+
+        summaries.forEach((summary) => {
+          const dtStart = new Date(0);
+          dtStart.setFullYear(month <= 3 ? year + 1 : year, month - 1, fromDay);
+          const dtEnd = new Date(0);
+          dtEnd.setFullYear(
+            toMonth <= 3 ? year + 1 : year,
+            toMonth - 1,
+            toDay + 1,
+          );
+
+          const event = new VEvent({ dtStart, dtEnd, summary, allDay: true });
+          events.push(event);
+        });
+      });
     });
   });
+  return { events, year };
+}
+
+async function scraping(oldCalendar?: VCalendar) {
+  const calendar = oldCalendar || createKosenCalendar("津山高専", "津山工業高等専門学校");
+
+  const { events: newEvents, year } = await getScrapeEvents();
+  const oldEvents = calendar.getEvents();
+
+  const allEvents = compEvents(oldEvents, newEvents, year);
+
+  calendar.setEvents(allEvents);
 
   return calendar;
 }
 
-const fileName = `tsuyama.ics`;
+const fileName = resolve(`./tsuyama.ics`);
 
 let text = "";
 try {
@@ -59,5 +106,5 @@ if (!calendar) {
 const icsText = calendar.toICSString();
 //console.log(icsText);
 
-//Deno.writeTextFileSync(fileName, icsText);
+Deno.writeTextFileSync(fileName, icsText);
 //console.log(`${fileName}に出力しました。`);
