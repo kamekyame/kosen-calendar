8c41e20d00bfa9602edf60e860888ab4a6d4e1a4
SuzuTomo <47011206+kamekyame@users.noreply.github.com>
2022-11-10 12:54:02 +0900

    fix(ci): PR作成を実行に失敗しても実行するように変更

--- update.ts
+++ update.ts
@@ -1,12 +1,19 @@
 import * as log from "https://deno.land/std@0.139.0/log/mod.ts";
 import { expandGlob } from "https://deno.land/std@0.139.0/fs/mod.ts";
 
+let isError = false;
+
 for await (const file of expandGlob("*/update*.ts")) {
   log.info(`Updating ${file.path}`);
 
   try {
     await import("file://" + file.path);
   } catch (e) {
+    isError = true;
     log.error(e);
   }
+}
+
+if (isError) {
+  Deno.exit(1);
 }
