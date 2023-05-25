5f85fd5a7349887b9525f61d1d146989e80ce3d9
kamekyame <kamekyame@outlook.com>
2023-05-25 10:30:43 +0900

    feat: add deno.json

--- script.sh
+++ script.sh
@@ -3,8 +3,8 @@ files="*/update*.ts"
 for filepath in $files; do
   echo  $filepath
   if [ "$1" = "update" ]; then
-    deno run -A $filepath
+    deno task update $filepath
   elif [ "$1" = "check" ]; then
-    deno check $filepath
+    deno task check $filepath
   fi
 done
